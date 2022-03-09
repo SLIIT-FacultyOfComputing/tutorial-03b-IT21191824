4c2948ef0e8f24d7e2179939ab66496eb82faddf
TMDSL Dias <99653011+IT21191824@users.noreply.github.com>
2022-03-09 20:30:46 +0000

    Done

--- exercise02.cpp
+++ exercise02.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 int volume(int height, int width, int length);
@@ -31,3 +32,8 @@ int main() {
 }
 
 // Implement the Volume() function here
+int volume(int height, int width, int length){
+  int totalVolume;
+  totalVolume=height*width*length;
+  return totalVolume;
+}
--- exercise04.cpp
+++ exercise04.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void print(int len, int wth);
-void input(int len, int wth);
+void input(int &len, int &wth);
 
 // Do not change the main() function
 int main() {
@@ -11,11 +11,15 @@ int main() {
    print(length, width);
    return 0;
 }
-
 // Do not change the print() function
 void print(int len, int wth) {
-   cout << "Length : " << len 
-        << ", Width  : " << wth << endl;
+    cout << "Length : " << len 
+         << ", Width  : " << wth << endl;
 }
-
 // Implement the Input Function here
+void input(int &len, int &wth){
+    cout << "Length :";
+    cin>>len;
+    cout << "Width :";
+    cin>>wth;
+}
